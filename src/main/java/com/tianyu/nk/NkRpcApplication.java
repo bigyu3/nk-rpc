1a0ac3a4d6839b261993eb821a7f89e71efc271a
tianyu1.guo <tianyu1.guo@tcl.com>
2024-01-24 19:54:34 +0800

    first

---
+++ src/main/java/com/tianyu/nk/NkRpcApplication.java
@@ -0,0 +1,13 @@
+package com.tianyu.nk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NkRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tianyu/nk/NkRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyu.nk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
